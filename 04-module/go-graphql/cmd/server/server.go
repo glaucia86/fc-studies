987ea8615a61b57bc6c4b84228617d3f9b911338
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-06-17 03:12:24 +0000

    chore: development persisting category via Playground

--- 04-module/go-graphql/cmd/server/server.go
+++ 04-module/go-graphql/cmd/server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
@@ -8,17 +9,31 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/devfullcycle/13-GraphQL/graph"
+	"github.com/devfullcycle/13-GraphQL/internal/database"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+
+	if err != nil {
+		log.Fatal("Failed to open database: %v", err)
+	}
+
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDB,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
--- 04-module/go-graphql/go.mod
+++ 04-module/go-graphql/go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/99designs/gqlgen v0.17.33
 	github.com/google/uuid v1.3.0
+	github.com/mattn/go-sqlite3 v1.14.17
 	github.com/vektah/gqlparser/v2 v2.5.3
 )
 
