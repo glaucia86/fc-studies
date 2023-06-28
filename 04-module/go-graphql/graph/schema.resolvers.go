d889a60ffa32900d4ab5bf632904059f20819090
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-06-28 19:24:32 -0300

    chore: development 'findAll' Courses

--- 04-module/go-graphql/graph/schema.resolvers.go
+++ 04-module/go-graphql/graph/schema.resolvers.go
@@ -6,7 +6,6 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/devfullcycle/13-GraphQL/graph/model"
 )
@@ -63,7 +62,22 @@ func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, erro
 
 // Courses is the resolver for the courses field.
 func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
-	panic(fmt.Errorf("not implemented: Courses - courses"))
+	courses, err := r.CourseDB.FindAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var coursesModel []*model.Course
+	for _, course := range courses {
+		coursesModel = append(coursesModel, &model.Course{
+			ID:          course.ID,
+			Name:        course.Name,
+			Description: &course.Description,
+		})
+	}
+
+	return coursesModel, nil
 }
 
 // Mutation returns MutationResolver implementation.
