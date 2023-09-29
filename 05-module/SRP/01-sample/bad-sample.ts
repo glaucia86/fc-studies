794f1c65306524d196abed805f7e08643c8e0668
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-09-29 00:03:00 -0300

    chore: sample usage of the SRP

---
+++ 05-module/SRP/01-sample/bad-sample.ts
@@ -0,0 +1,23 @@
+/**
+ * file: srp/01-sample/bad-sample.ts
+ * description: file responsible for sample of bad sample applied to SRP - Single Responsability Principle
+ * data: 09/28/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+export { };
+
+class Student {
+  public createStudentAccount() {
+    // alguma lógica aqui	
+  }
+
+  public calculateStudentGrade() {
+    // alguma lógica aqui
+  }
+
+  public generateStudentData() {
+    // alguma lógica aqui
+  }
+}
+
